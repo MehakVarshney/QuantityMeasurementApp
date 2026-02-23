df668f72599d188d2df28262818016402301c458
MehakVarhney <mehakvarshney1320@gmail.com>
2026-02-23 18:44:55 +0530

    [MehakVarshney] Add . UC 2 for Quanlity Measurement App

--- src/main/java/com/QuantityMeasurementApp/QuantityMeasurementApp.java
+++ src/main/java/com/QuantityMeasurementApp/QuantityMeasurementApp.java
@@ -49,17 +49,6 @@ public class QuantityMeasurementApp {
                 Inches other = (Inches) obj;
                 return Double.compare(this.value, other.value) == 0;
             }
-            public static boolean checkFeetEquality(double value1, double value2) {
-                Feet feet1 = new Feet(value1);
-                Feet feet2 = new Feet(value2);
-                return feet1.equals(feet2);
-            }
-
-            public static boolean checkInchesEquality(double value1, double value2) {
-                Inches inch1 = new Inches(value1);
-                Inches inch2 = new Inches(value2);
-                return inch1.equals(inch2);
-            }
         }
         
         public static void main(String[] args) {
