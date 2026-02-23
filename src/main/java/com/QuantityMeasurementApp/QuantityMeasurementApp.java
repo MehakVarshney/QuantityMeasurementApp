6093b6b18285001dc985f79ba248be6483883166
MehakVarhney <mehakvarshney1320@gmail.com>
2026-02-23 19:21:10 +0530

    [MehakVarshney] Add . UC3 for Quantity Measurement App

--- src/main/java/com/QuantityMeasurementApp/QuantityMeasurementApp.java
+++ src/main/java/com/QuantityMeasurementApp/QuantityMeasurementApp.java
@@ -24,10 +24,11 @@ public class QuantityMeasurementApp {
 
             return Double.compare(this.value, other.value) == 0;
         }
+	}
         
         // UC - 2 
         
-        public class Inches {
+        public static class Inches {
 
             private final double value;
 
@@ -48,17 +49,6 @@ public class QuantityMeasurementApp {
 
                 Inches other = (Inches) obj;
                 return Double.compare(this.value, other.value) == 0;
-            }
-            public static boolean checkFeetEquality(double value1, double value2) {
-                Feet feet1 = new Feet(value1);
-                Feet feet2 = new Feet(value2);
-                return feet1.equals(feet2);
-            }
-
-            public static boolean checkInchesEquality(double value1, double value2) {
-                Inches inch1 = new Inches(value1);
-                Inches inch2 = new Inches(value2);
-                return inch1.equals(inch2);
             }
         }
         
@@ -144,5 +134,4 @@ public class QuantityMeasurementApp {
             System.out.println("Input: 1.0 ft and 12.0 inch");
             System.out.println("Output: Equal (" + result + ")");
         }
-    }
 }
