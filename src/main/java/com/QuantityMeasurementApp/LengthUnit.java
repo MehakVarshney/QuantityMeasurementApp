9481d4bdc6281063b7893da0dc8ceb9444bb7596
MehakVarhney <mehakvarshney1320@gmail.com>
2026-03-26 17:09:02 +0530

    [MehakVarshney] . Update UC13

--- src/main/java/com/QuantityMeasurementApp/LengthUnit.java
+++ src/main/java/com/QuantityMeasurementApp/LengthUnit.java
@@ -1,6 +1,7 @@
 package com.QuantityMeasurementApp;
 
 public enum LengthUnit implements IMeasurable {
+
 	FEET(1.0), INCH(1.0 / 12.0), YARDS(3.0), CENTIMETER(0.393701 / 12.0);
 
 	private final double factorToFeet;
--- src/main/java/com/QuantityMeasurementApp/VolumeUnit.java
+++ src/main/java/com/QuantityMeasurementApp/VolumeUnit.java
@@ -1,6 +1,7 @@
 package com.QuantityMeasurementApp;
 
 public enum VolumeUnit implements IMeasurable {
+
 	LITRE(1.0), MILLILITRE(0.001), GALLON(3.78541);
 
 	private final double factor;
